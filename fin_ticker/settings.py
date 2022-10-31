b0578e828d4f8a951489fdd55a4c1eb64fe5b35a
Kobell <50255183+jkobell@users.noreply.github.com>
2022-10-31 08:40:25 -0500

    Update settings.py

--- fin_ticker/settings.py
+++ fin_ticker/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$d1-nhhme!#4#iqa9@@8y0lf$5iqv&6=3dl2+a0)-xfe_=5ykn'
+SECRET_KEY = 'place-key-here'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
