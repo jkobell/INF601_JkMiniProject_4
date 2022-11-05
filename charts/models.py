46cc125c820cb03abc91f7d61cf793afe0349588
jk185256 <jk185256@ncr.com>
2022-11-05 16:20:39 -0500

    added_ticker_model

--- charts/models.py
+++ charts/models.py
@@ -1,3 +1,20 @@
+from operator import mod
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+class Ticker(models.Model):
+    is_active = models.BooleanField()
+    symbol = models.CharField(max_length=5)#NYSE 1-5 chars long
+    name = models.CharField(max_length=150)
+    exchange = models.CharField(max_length=25)
+    currency_code = models.CharField(max_length=3)#ISO 4217
+    currency_country = models.CharField(max_length=25)
+    currency_name = models.CharField(max_length=50)
+    currency_number = models.PositiveSmallIntegerField()
+    industry = models.CharField(max_length=25)
+    sector = models.CharField(max_length=25)
+    description = models.CharField(max_length=250)
+    
+
+
--- charts/views.py
+++ charts/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the CHARTS index.")
+    return HttpResponse("Hello, world. You're at the CHARTS index - for now :) - right?.")
