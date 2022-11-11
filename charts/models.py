83d2931e71f0672d13e8661f2901079c450f484b
jk185256 <jk185256@ncr.com>
2022-11-11 14:11:57 -0600

    add_object_label_name

--- charts/models.py
+++ charts/models.py
@@ -5,20 +5,41 @@ class Timezone(models.Model):
     abbr = models.CharField(max_length=10)
     abbr_dst = models.CharField(max_length=10)
 
+    def __str__(self):
+        return 'Timezone: ' + self.timezone
+
 class Currency(models.Model):
     code = models.CharField(max_length=15)#
     symbol = models.CharField(max_length=25)
     name = models.CharField(max_length=50)
 
-class Ticker(models.Model):
+    def __str__(self):
+        return 'Currency: ' + self.name
+
+class Exchange(models.Model):
     timezone = models.ForeignKey(Timezone, on_delete=models.CASCADE)
     currency = models.ForeignKey(Currency, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)#
+    acronym = models.CharField(max_length=25)
+    market_id = models.CharField(max_length=25)
+    country = models.CharField(max_length=25)
+    country_code = models.CharField(max_length=25)
+    city = models.CharField(max_length=25)
+    website = models.CharField(max_length=25)
+
+    def __str__(self):
+        return 'Exchange: ' + self.name
+
+class Ticker(models.Model):
+    exchange = models.ForeignKey(Exchange, on_delete=models.CASCADE)    
     is_active = models.BooleanField()
     name = models.CharField(max_length=150)
     symbol = models.CharField(max_length=10)
     has_intraday = models.BooleanField() 
     has_eod = models.BooleanField()
-    stock_exchange = models.CharField(max_length=25)
+
+    def __str__(self):
+        return 'Ticker: ' + self.name
     
 class Chart(models.Model):
     ticker = models.ForeignKey(Ticker, on_delete=models.CASCADE)
@@ -29,14 +50,8 @@ class Chart(models.Model):
     bg_color = models.CharField(max_length=25)
     curve_color = models.CharField(max_length=25)
 
-class Exchange(models.Model):
-    name = models.CharField(max_length=50)#
-    acronym = models.CharField(max_length=25)
-    market_id = models.CharField(max_length=25)
-    country = models.CharField(max_length=25)
-    country_code = models.CharField(max_length=25)
-    city = models.CharField(max_length=25)
-    website = models.CharField(max_length=25)
+    def __str__(self):
+        return 'Chart: ' + self.name
 
 class Intraday(models.Model):
     ticker = models.ForeignKey(Ticker, on_delete=models.CASCADE)
@@ -47,8 +62,9 @@ class Intraday(models.Model):
     country_code = models.CharField(max_length=25)
     city = models.CharField(max_length=25)
     datetime = models.DateTimeField()
-    #symbol = models.CharField(max_length=10)
-    #stock_exchange = models.CharField(max_length=25)
+    
+    def __str__(self):
+        return 'Intraday: ' + self.name
 
 
     
