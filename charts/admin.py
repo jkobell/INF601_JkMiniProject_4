b0979326cae26c54f59808b44b5f41a477ac818f
jk185256 <jk185256@ncr.com>
2022-11-11 13:06:56 -0600

    add_exchange_intraday_currency_timezone

--- charts/admin.py
+++ charts/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Ticker
+from .models import Chart
+from .models import Exchange
+from .models import Timezone
+from .models import Currency
+from .models import Intraday
+
+admin.site.register(Ticker)
+admin.site.register(Chart)
+admin.site.register(Exchange)
+admin.site.register(Timezone)
+admin.site.register(Currency)
+admin.site.register(Intraday)
+
+
--- charts/models.py
+++ charts/models.py
@@ -1,19 +1,25 @@
 from django.db import models
 
-# Create your models here.
-class Ticker(models.Model):
-    is_active = models.BooleanField()
-    symbol = models.CharField(max_length=5)#NYSE 1-5 chars long
-    name = models.CharField(max_length=150)
-    exchange = models.CharField(max_length=25)
-    currency_code = models.CharField(max_length=3)#ISO 4217
-    currency_country = models.CharField(max_length=25)
-    currency_name = models.CharField(max_length=50)
-    currency_number = models.PositiveSmallIntegerField()
-    industry = models.CharField(max_length=25)
-    sector = models.CharField(max_length=25)
-    description = models.CharField(max_length=250)
+class Timezone(models.Model):
+    timezone = models.CharField(max_length=25)
+    abbr = models.CharField(max_length=10)
+    abbr_dst = models.CharField(max_length=10)
 
+class Currency(models.Model):
+    code = models.CharField(max_length=15)#
+    symbol = models.CharField(max_length=25)
+    name = models.CharField(max_length=50)
+
+class Ticker(models.Model):
+    timezone = models.ForeignKey(Timezone, on_delete=models.CASCADE)
+    currency = models.ForeignKey(Currency, on_delete=models.CASCADE)
+    is_active = models.BooleanField()
+    name = models.CharField(max_length=150)
+    symbol = models.CharField(max_length=10)
+    has_intraday = models.BooleanField() 
+    has_eod = models.BooleanField()
+    stock_exchange = models.CharField(max_length=25)
+    
 class Chart(models.Model):
     ticker = models.ForeignKey(Ticker, on_delete=models.CASCADE)
     is_active = models.BooleanField()
@@ -22,6 +28,27 @@ class Chart(models.Model):
     end_date = models.DateField()
     bg_color = models.CharField(max_length=25)
     curve_color = models.CharField(max_length=25)
+
+class Exchange(models.Model):
+    name = models.CharField(max_length=50)#
+    acronym = models.CharField(max_length=25)
+    market_id = models.CharField(max_length=25)
+    country = models.CharField(max_length=25)
+    country_code = models.CharField(max_length=25)
+    city = models.CharField(max_length=25)
+    website = models.CharField(max_length=25)
+
+class Intraday(models.Model):
+    ticker = models.ForeignKey(Ticker, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)#
+    acronym = models.CharField(max_length=25)
+    market_id = models.CharField(max_length=25)
+    country = models.CharField(max_length=25)
+    country_code = models.CharField(max_length=25)
+    city = models.CharField(max_length=25)
+    datetime = models.DateTimeField()
+    #symbol = models.CharField(max_length=10)
+    #stock_exchange = models.CharField(max_length=25)
 
 
     
