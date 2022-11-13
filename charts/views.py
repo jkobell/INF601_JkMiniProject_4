030f58cd526f0e8c8c92cf807e90291143e61580
jk185256 <jk185256@ncr.com>
2022-11-13 10:56:36 -0600

    add_try_except

--- charts/views.py
+++ charts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import render
 from django.http import Http404
 from django.http import HttpResponse
 from django.template import loader
@@ -6,14 +6,9 @@ from .models import Exchange
 from .models import Currency
 from .models import Ticker
 
-# Create your views here.
+#index list view
 def index(request):
-    #return HttpResponse("Hello, world. You're at the CHARTS index - for now :) - right?.")
-
-    #pages_list = ['exchanges', 'tickers', 'charts', 'currency']
     pages_list = ['exchange', 'currency', 'ticker']
-    #exchange_list = ["red", "white", "blue"]
-    #template = loader.get_template('charts/index.html')
     context = {
         'page_name': 'Site Index',
         'pages_list': pages_list,
@@ -27,49 +22,65 @@ def exchange(request):
             'exchange_list': exchange_list,
             'page_name': 'Available Exchanges',
             'exchange_detail': 'exchange_detail',
-        }
+        }       
+        #raise Http404 #uncomment to test try/except
     except Exchange.DoesNotExist:
-        raise Http404("Exchange does not exist")
+        raise Http404() #if settings DEBUG=False, browser displays: Not Found The requested resource was not found on this server. [preferred]
     return render(request, 'charts/exchange.html', context=context)
 
 def exchange_detail(request, exchange_id):
-    exchange_content = Exchange.objects.filter(id=exchange_id)
-    context = {
-        'exchange_content': exchange_content,
-        'page_name': 'Exchange Details',
-    }
+    try:
+        exchange_content = Exchange.objects.filter(id=exchange_id)
+        context = {
+            'exchange_content': exchange_content,
+            'page_name': 'Exchange Details',
+        }
+    except Exchange.DoesNotExist:
+        raise Http404()
     return render(request, 'charts/exchange_detail.html', context=context)
 
 def currency(request):
-    currency_list = Currency.objects.all()
-    context = {
-        'currency_list': currency_list,
-        'page_name': 'Available Currencies',
-        'currency_detail': 'currency_detail',
-    }
+    try:
+        currency_list = Currency.objects.all()
+        context = {
+            'currency_list': currency_list,
+            'page_name': 'Available Currencies',
+            'currency_detail': 'currency_detail',
+        }
+    except Exchange.DoesNotExist:
+        raise Http404()
     return render(request, 'charts/currency.html', context=context)
 
 def currency_detail(request, currency_id):
-    currency_content = Currency.objects.filter(id=currency_id)
-    context = {
-        'currency_content': currency_content,
-        'page_name': 'Currency Details',
-    }
+    try:
+        currency_content = Currency.objects.filter(id=currency_id)
+        context = {
+            'currency_content': currency_content,
+            'page_name': 'Currency Details',
+        }
+    except Exchange.DoesNotExist:
+        raise Http404()
     return render(request, 'charts/currency_detail.html', context=context)
 
 def ticker(request):
-    ticker_list = Ticker.objects.all()
-    context = {
-        'ticker_list': ticker_list,
-        'page_name': 'Available Tickers',
-        'ticker_detail': 'ticker_detail',
-    }
+    try:
+        ticker_list = Ticker.objects.all()
+        context = {
+            'ticker_list': ticker_list,
+            'page_name': 'Available Tickers',
+            'ticker_detail': 'ticker_detail',
+        }
+    except Exchange.DoesNotExist:
+        raise Http404()
     return render(request, 'charts/ticker.html', context=context)
 
 def ticker_detail(request, ticker_id):
-    ticker_content = Ticker.objects.filter(id=ticker_id)
-    context = {
-        'ticker_content': ticker_content,
-        'page_name': 'Ticker Details',
-    }
+    try:
+        ticker_content = Ticker.objects.filter(id=ticker_id)
+        context = {
+            'ticker_content': ticker_content,
+            'page_name': 'Ticker Details',
+        }
+    except Exchange.DoesNotExist:
+        raise Http404()
     return render(request, 'charts/ticker_detail.html', context=context)
--- fin_ticker/settings.py
+++ fin_ticker/settings.py
@@ -22,9 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'place-key-here'
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+#DEBUG = False
 
+#if localhost and DEBUG=True
+#ALLOWED_HOSTS = ['localhost', '127.0.0.1',]
+
+#if DEBUG=False
 ALLOWED_HOSTS = []
 
 
